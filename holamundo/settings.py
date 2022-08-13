1f0b9e7bbd5e4ae414c8c443cf58c0c661e3fcce
Sebastián Catalán <sicatalan@uc.cl>
2022-08-13 14:45:01 -0400

    first template

--- holamundo/settings.py
+++ holamundo/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'holamundo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'], #esto hay que agregarlo
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
---
+++ holamundo/views.py
@@ -0,0 +1,19 @@
+from django.http import HttpResponse
+
+
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def saludo(request): #always recieve a request
+    return HttpResponse("Hola mundo")
+
+def adulto(request, edad): #en este ejemplo se recibe un parametro en la vista
+    if edad >= 18:
+        return HttpResponse("Eres mayor de edad")
+    else:
+        return HttpResponse("No eres mayor de edad")
+
+def simple(request):
+    return render(request, 'simple.html', {}) 
+    #información de la request, que plantilla, contexto (plantillas pueden necesitar datos)
+    #en este caso es un contexto vacío
